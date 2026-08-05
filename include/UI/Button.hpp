a7e004401432ce0501e24f3995bfb2879ba90c79
G4vi1 <gabrielvictoy7@gmail.com>
2026-08-05 13:38:28 -0300

    Buttons settings

---
+++ include/UI/Button.hpp
@@ -0,0 +1,34 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include "Renderer.hpp"
+
+class Button
+{
+public:
+
+    // Constructor
+
+    Button(float x,
+           float y,
+           float width,
+           float height);
+
+    // Draws the button.
+
+    void draw(Renderer& renderer);
+
+private:
+
+    // Button position.
+
+    float x;
+    float y;
+
+    // Button size.
+
+    float width;
+    float height;
+};
+
+#endif
--- src/UI/Button.cpp
+++ src/UI/Button.cpp
@@ -1,1 +1,43 @@
 #include "UI/Button.hpp"
+
+// Constructor.
+//
+// Stores the position and dimensions
+// of the button.
+
+Button::Button(float x,
+               float y,
+               float width,
+               float height)
+    : x(x),
+      y(y),
+      width(width),
+      height(height)
+{
+}
+
+// Draws the button.
+//
+// For now, a button is simply a
+// light gray rectangle.
+
+void Button::draw(Renderer& renderer)
+{
+    // Selects the button color.
+
+    renderer.setDrawColor(
+        140,
+        140,
+        140,
+        255
+    );
+
+    // Draws the button body.
+
+    renderer.drawFilledRect(
+        x,
+        y,
+        width,
+        height
+    );
+}
